925770e93927d5763610a4280be5a65e8f1cfa86
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-15 12:48:42 +0530

    updated models and components

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,11 @@
+module Api
+    module V1
+      class  GreetingsController < ApplicationController
+        protect_from_forgery with: :null_session
+          def show_greeting
+            greeting = Greeting.order('RANDOM()').first
+            render json: { message: greeting.message }
+          end
+      end
+    end
+end
